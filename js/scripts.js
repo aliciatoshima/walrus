02e50856108dad953b8739104fb19768e5cd56ed
Alicia Toshima and Emily Bobbe <emily@>
2019-02-04 19:00:31 -0800

    Add sand castle text and style.

--- js/scripts.js
+++ js/scripts.js
@@ -3,4 +3,9 @@ $(document).ready(function() {
     $("#walrus-showing").toggle();
     $("#walrus-hidden").toggle();
   });
+
+  $(".clickable1").click(function() {
+    $("#castle-hidden").fadeToggle();
+    $("#castle-showing").fadeToggle();
+  });
 });
