2bc8eaeaff43b8a55eb91189090cc50eaf4589e9
Alicia Toshima and Emily Bobbe <emily@>
2019-02-04 19:47:29 -0800

    Add buttons and colors.

--- js/scripts.js
+++ js/scripts.js
@@ -8,4 +8,26 @@ $(document).ready(function() {
     $("#castle-hidden").fadeToggle();
     $("#castle-showing").fadeToggle();
   });
+
+  $(".clickable2").click(function() {
+    $("#seashell-hidden").slideToggle();
+    $("#seashell-showing").slideToggle();
+  });
+
+  $("button#dark").click(function() {
+    $("body").removeClass();
+    $("body").addClass("dark-background")
+    $("body").addClass("light-text");
+  });
+
+  $("button#medium").click(function() {
+    $("body").removeClass();
+    $("body").addClass("medium-background")
+    $("body").addClass("medium-text")
+  });
+
+  $("button#light").click(function() {
+    $("body").removeClass();
+    $("body").addClass("light-background");
+  });
 });
